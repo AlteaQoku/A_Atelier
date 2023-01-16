177b3bb268100848533c82db9d306f308ae60400
Altea Qoku <qokualtea8@gmail.com>
2023-01-16 09:59:35 +0100

    Add project files.

---
+++ A_Atelier/Controllers/ShopController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace A_Atelier.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
